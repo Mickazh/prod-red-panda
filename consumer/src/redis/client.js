4be6a00319a077d68486e32d373416457f0336d0
Mickazh <127355080+Mickazh@users.noreply.github.com>
2025-02-07 16:41:13 +0100

    etape 3

--- consumer/src/redis/client.js
+++ consumer/src/redis/client.js
@@ -1,7 +1,8 @@
 import { createClient } from 'redis'
 
 export const redisClient = await createClient({
-  // url: 'redis://localhost:6379',
+  url: "redis://localhost:6379",
+  password: "redispwd",
 })
   .on("error", (err) => console.log("Redis Client Error", err))
   .connect();
--- consumer/src/server.js
+++ consumer/src/server.js
@@ -1,25 +1,8 @@
-import {
-  getConfigNumber,
-  getDebug,
-  getTopic,
-  getTypeMessage,
-} from "./config/config.js";
 import "dotenv/config";
 import { connection } from "./redpanda/consumer.js";
-import { redisClient } from "./redis/client.js";
 
 async function start() {
   connection();
 }
-import { createClient } from 'redis';
-
-const client = await createClient()
-  .on('error', err => console.log('Redis Client Error', err))
-  .connect();
-
-await client.set('key', 'value');
-const value = await client.get('key');
-await client.disconnect();
-// console.log(await redisClient.get("test"));
 
 start();
